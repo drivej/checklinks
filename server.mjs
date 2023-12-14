495b6dddbe9716c81ba9a26a8c33dd29cabf3cfa
drivej <drivej@hotmail.com>
2023-12-14 13:39:47 -0500

    x

--- server.mjs
+++ server.mjs
@@ -1,25 +1,25 @@
 import express from 'express';
 import path from 'path';
-import { runJob, createJob, getJob, stopJob } from './checklinks.mjs';
+// import { runJob, createJob, getJob, stopJob } from './checklinks.mjs';
 
 const app = express();
 const PORT = process.env.PORT || 3010;
 
 app.use(express.static('dist'));
 
-app.get('/checklinks', async (req, res) => {
-  const job = createJob(req.query);
-  runJob(job.id);
-  res.json(job);
-});
+// app.get('/checklinks', async (req, res) => {
+//   const job = createJob(req.query);
+//   runJob(job.id);
+//   res.json(job);
+// });
 
-app.get('/job/:id/stop', async (req, res) => {
-  res.json(stopJob(req.params.id));
-});
+// app.get('/job/:id/stop', async (req, res) => {
+//   res.json(stopJob(req.params.id));
+// });
 
-app.get('/job/:id', async (req, res) => {
-  res.json(getJob(req.params.id));
-});
+// app.get('/job/:id', async (req, res) => {
+//   res.json(getJob(req.params.id));
+// });
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve('./dist/index.html'));
