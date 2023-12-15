7794464e9cd393d2b7e48dd7856bd437956d9a24
drivej <drivej@hotmail.com>
2023-12-15 08:00:02 -0500

    x

--- server.mjs
+++ server.mjs
@@ -7,19 +7,19 @@ const PORT = process.env.PORT || 3010;
 
 app.use(express.static('dist'));
 
-// app.get('/checklinks', async (req, res) => {
-//   const job = createJob(req.query);
-//   runJob(job.id);
-//   res.json(job);
-// });
+app.get('/checklinks', async (req, res) => {
+  const job = createJob(req.query);
+  runJob(job.id);
+  res.json(job);
+});
 
-// app.get('/job/:id/stop', async (req, res) => {
-//   res.json(stopJob(req.params.id));
-// });
+app.get('/job/:id/stop', async (req, res) => {
+  res.json(stopJob(req.params.id));
+});
 
-// app.get('/job/:id', async (req, res) => {
-//   res.json(getJob(req.params.id));
-// });
+app.get('/job/:id', async (req, res) => {
+  res.json(getJob(req.params.id));
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve('./dist/index.html'));
